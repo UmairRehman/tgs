afa88712db8fdae068fa51970c8bc74cf502143d
syedahmedhaider <ahmed.haider@liqteq.com>
2021-10-27 15:11:36 +0500

    INTERIM | Employee Statuses

---
+++ src/Imports/employeeStatuses.js
@@ -0,0 +1,39 @@
+export const employeeStatuses = [
+    {
+        id: 1,
+        name: "Step 0 Submit"
+    },
+    {
+        id: 2,
+        name: "Step 0 Approved"
+    },
+    {
+        id: 3,
+        name: "Step 0 Reject"
+    },
+    {
+        id: 4,
+        name: "Step 1 Submit"
+    },
+    {
+        id: 5,
+        name: "Step 1 Approved"
+    },
+    {
+        id: 6,
+        name: "Step 1 Reject"
+    },
+    {
+        id: 7,
+
+        name: "Step 2 Submit"
+    },
+    {
+        id: 8,
+        name: "Step 3 Submit"
+    },
+    {
+        id: 9,
+        name: "Employee"
+    },
+]
--- src/Imports/index.js
+++ src/Imports/index.js
@@ -1,7 +1,9 @@
 const { statics } = require('./statics');
 const { validators } = require('./validators');
+const { employeeStatuses } = require('./employeeStatuses');
 
 export const Imports = {
     ...statics,
     ...validators,
+    employeeStatuses,
 }
