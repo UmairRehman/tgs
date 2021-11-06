0c5d602e7602a3b47bc5737683f1bab1994fbce3
syedahmedhaider <ahmed.haider@liqteq.com>
2021-11-06 20:03:57 +0500

    INTERIM | Create Password checks asserted

--- src/Pages/Applicant/Application/CreatePassword.js
+++ src/Pages/Applicant/Application/CreatePassword.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   Grid,
   TextField,
@@ -21,13 +21,27 @@ import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 // import MobileScreen from '../Start/Mobile/Login';
 // import {isMobile} from 'react-device-detect';
-import Services from '../../../Services';
 import { useHistory } from "react-router-dom";
+
+
+/** Local dependeinces & Libraries */
+import Services from '../../../Services';
+
+import { helpers } from '../../../helpers';
+
+import Snackbar from '../../../Components/Snackbar';
+
+import { Imports } from '../../../Imports';
 
 
 const {
   users
 } = Services;
+
+const {
+  showSnackBar,
+} = helpers;
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -50,27 +64,46 @@ const CreatePassword = () => {
   const [error, setError] = useState(false);
   const [token, setToken] = useState(localStorage.getItem('access_jwt'))
 
-  async function submit(){
+  /**
+   * @param {string} password - Password to validate 
+   * @returns {string|Error}
+   */
+  const validatePassword = async (password) => {
+    try {
+      return await Imports
+        .password
+        .validate(password);
+    } catch (exc) {
+      showSnackBar(exc.message);
+      throw exc;
+    }
+  }
+
+  async function submit() {
     let data = {
       password: password,
-      confirm_password : confirmPassword
+      confirm_password: confirmPassword
     }
-    if(password === confirmPassword){
+    if (password === confirmPassword) {
       setError(false)
-      try{
+      try {
+        const isValid = await validatePassword(password);
+
         const createPassword = await users.createPassword(data)
-        if(createPassword.message == 'success')
+        if (createPassword.message == 'success')
           history.push("/");
       }
-      catch(exc){
+      catch (exc) {
         console.log(exc);
       }
     }
-    else{
+    else {
       setError(true);
+
+      return false;
     }
   }
-  
+
   const gotoDashBoard = () => {
     window.location = '/dashboard';
   };
@@ -80,7 +113,7 @@ const CreatePassword = () => {
 
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
-  
+
   const [values, setValues] = React.useState({
     amount: '',
     password: '',
@@ -110,109 +143,110 @@ const CreatePassword = () => {
     event.preventDefault();
   };
 
-//   if(isMobile) {
-//     return (
-//         <MobileScreen />
-//     )
-//   }
+  //   if(isMobile) {
+  //     return (
+  //         <MobileScreen />
+  //     )
+  //   }
   return (
     <Grid container xs={12}>
       <Grid className="LoginImageBG">
-          <Grid className="LoginImage">
-              <Grid className="LoginLogo"></Grid>
-              <Grid className="LoginText">
-                Applicant Portal
-              </Grid>
+        <Grid className="LoginImage">
+          <Grid className="LoginLogo"></Grid>
+          <Grid className="LoginText">
+            Applicant Portal
           </Grid>
+        </Grid>
       </Grid>
       <Grid className="LoginFormPlace CreatePwdForm">
-          <Grid className="LoginForm">
-              <Grid className="LoginTitle">Create Password</Grid>
-              <Grid className="FormFields">
-                  <Typography xs={12} className="CreateFieldTItle">Create Password</Typography>
-                  <FormControl className="LoginPwd">
-                    {/* <InputLabel htmlFor="standard-adornment-password">Password</InputLabel> */}
-                    <Input
-                    placeholder="Password"
-                      id="standard-adornment-password"
-                      type={values.showPassword ? 'text' : 'password'}
-                      value={password}
-                      onChange={ e =>setPassword(e.target.value)}
-                      endAdornment={
-                        <InputAdornment position="end">
-                          <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                            onMouseDown={handleMouseDownPassword}
-                          >
-                            {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                          </IconButton>
-                        </InputAdornment>
-                      }
-                    />
-                  </FormControl>
-                  <Typography xs={12} className="CreateFieldTItle">Verify Password</Typography>
-                  <FormControl className="LoginPwd">
-                    {/* <InputLabel htmlFor="standard-adornment-password">Password</InputLabel> */}
-                    <Input
-                    placeholder="Verify Password"
-                      id="standard-adornment-password"
-                      type={values.showPassword ? 'text' : 'password'}
-                      value={confirmPassword}
-                      onChange={ e => setConfirmPassword(e.target.value)}
-                      endAdornment={
-                        <InputAdornment position="end">
-                          <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPasswordV}
-                            onMouseDown={handleMouseDownPasswordV}
-                          >
-                            {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                          </IconButton>
-                        </InputAdornment>
-                      }
-                    />
-                  </FormControl>
-                  {error == true?
-                    <h5>Paswword does not match</h5>
-                  :"" }
-                  <Typography xs={12} className="CreateFieldTItle">Verify Password</Typography>
-                  {/* <TextField placeholder="Password" type="password"/> */}
-                  <Grid xs={12} container className="mt16 pwdcriteria">
-                    <Typography variant="h5" component="h6">
-                      Password Criteria
-                    </Typography>
-                    <List component="nav" aria-label="secondary mailbox folders">
-                      <ListItem>
-                        <ListItemText primary="Must be eight or more characters long" />
-                      </ListItem>
-                      <ListItem>
-                        <ListItemText primary="Password must contain two of the following four categories" />
-                      </ListItem>
-                      <ListItem>
-                        <List component="nav" aria-label="secondary mailbox folders">
-                          <ListItem>
-                            <ListItemText primary="Upper Case Characters A-Z" />
-                          </ListItem>
-                          <ListItem>
-                            <ListItemText primary="Lower Case Characters a-z" />
-                          </ListItem>
-                          <ListItem>
-                            <ListItemText primary="Digits 0-9" />
-                          </ListItem>
-                          <ListItem>
-                            <ListItemText primary="Special Characters (!,$,#,%,etc)" />
-                          </ListItem>
-                        </List>
-                      </ListItem>
-                    </List>
-                  </Grid>
-                  <Grid xs={12} container justify="center" className="mt40 mb40">
-                      <Button onClick={submit} className="ApplicantBtn">Login</Button>
-                      {/* to="/questionnaire" */}
-                  </Grid>
-              </Grid>
+        <Grid className="LoginForm">
+          <Grid className="LoginTitle">Create Password</Grid>
+          <Grid className="FormFields">
+            <Typography xs={12} className="CreateFieldTItle">Create Password</Typography>
+            <FormControl className="LoginPwd">
+              {/* <InputLabel htmlFor="standard-adornment-password">Password</InputLabel> */}
+              <Input
+                placeholder="Password"
+                id="standard-adornment-password"
+                type={values.showPassword ? 'text' : 'password'}
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <Typography xs={12} className="CreateFieldTItle">Verify Password</Typography>
+            <FormControl className="LoginPwd">
+              {/* <InputLabel htmlFor="standard-adornment-password">Password</InputLabel> */}
+              <Input
+                placeholder="Verify Password"
+                id="standard-adornment-password"
+                type={values.showPassword ? 'text' : 'password'}
+                value={confirmPassword}
+                onChange={e => setConfirmPassword(e.target.value)}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPasswordV}
+                      onMouseDown={handleMouseDownPasswordV}
+                    >
+                      {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            {error == true ?
+              <h5>Paswword does not match</h5>
+              : ""}
+            <Typography xs={12} className="CreateFieldTItle">Verify Password</Typography>
+            {/* <TextField placeholder="Password" type="password"/> */}
+            <Grid xs={12} container className="mt16 pwdcriteria">
+              <Typography variant="h5" component="h6">
+                Password Criteria
+              </Typography>
+              <List component="nav" aria-label="secondary mailbox folders">
+                <ListItem>
+                  <ListItemText primary="Must be eight or more characters long" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="Password must contain two of the following four categories" />
+                </ListItem>
+                <ListItem>
+                  <List component="nav" aria-label="secondary mailbox folders">
+                    <ListItem>
+                      <ListItemText primary="Upper Case Characters A-Z" />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemText primary="Lower Case Characters a-z" />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemText primary="Digits 0-9" />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemText primary="Special Characters (!,$,#,%,etc)" />
+                    </ListItem>
+                  </List>
+                </ListItem>
+              </List>
+            </Grid>
+            <Grid xs={12} container justify="center" className="mt40 mb40">
+              <Button onClick={submit} className="ApplicantBtn">Login</Button>
+              <Snackbar
+              ></Snackbar>
+            </Grid>
           </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
