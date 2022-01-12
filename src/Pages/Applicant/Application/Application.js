05ec1f46c660d976e5fd3e5ca25532733d208719
maazkhanLiqteq <maaz.khan@liqteq.com>
2022-01-12 16:56:59 +0500

    TGS | Applicant Page 1 | 50 States added

--- src/Pages/Applicant/Application/Application.js
+++ src/Pages/Applicant/Application/Application.js
@@ -49,28 +49,244 @@ const FullTitle = [
 ];
 
 const addressstate = [
-    { title: 'Alaska' },
-    { title: 'Arizona' },
-    { title: 'Arkansas' },
-    { title: 'California' },
-    { title: 'Colorado' },
-    { title: 'Connecticut' },
-    { title: 'Delaware' },
-    { title: 'Florida' },
-    { title: 'Georgia' },
-    { title: 'Hawaii' },
-    { title: 'Idaho' },
-    { title: 'Illinois' },
-    { title: 'Indiana' },
-    { title: 'Iowa' },
-    { title: 'Kansas' },
-    { title: 'Kentucky' },
-    { title: 'Louisiana' },
-    { title: 'Maine' },
-    { title: 'Maryland' },
-    { title: 'Massachusetts' },
-    { title: 'Michigan' }
+    {
+        title: "Alabama",
+        abbreviation: "AL"
+    },
+    {
+        title: "Alaska",
+        abbreviation: "AK"
+    },
+    {
+        title: "American Samoa",
+        abbreviation: "AS"
+    },
+    {
+        title: "Arizona",
+        abbreviation: "AZ"
+    },
+    {
+        title: "Arkansas",
+        abbreviation: "AR"
+    },
+    {
+        title: "California",
+        abbreviation: "CA"
+    },
+    {
+        title: "Colorado",
+        abbreviation: "CO"
+    },
+    {
+        title: "Connecticut",
+        abbreviation: "CT"
+    },
+    {
+        title: "Delaware",
+        abbreviation: "DE"
+    },
+    {
+        title: "District Of Columbia",
+        abbreviation: "DC"
+    },
+    {
+        title: "Federated States Of Micronesia",
+        abbreviation: "FM"
+    },
+    {
+        title: "Florida",
+        abbreviation: "FL"
+    },
+    {
+        title: "Georgia",
+        abbreviation: "GA"
+    },
+    {
+        title: "Guam",
+        abbreviation: "GU"
+    },
+    {
+        title: "Hawaii",
+        abbreviation: "HI"
+    },
+    {
+        title: "Idaho",
+        abbreviation: "ID"
+    },
+    {
+        title: "Illinois",
+        abbreviation: "IL"
+    },
+    {
+        title: "Indiana",
+        abbreviation: "IN"
+    },
+    {
+        title: "Iowa",
+        abbreviation: "IA"
+    },
+    {
+        title: "Kansas",
+        abbreviation: "KS"
+    },
+    {
+        title: "Kentucky",
+        abbreviation: "KY"
+    },
+    {
+        title: "Louisiana",
+        abbreviation: "LA"
+    },
+    {
+        title: "Maine",
+        abbreviation: "ME"
+    },
+    {
+        title: "Marshall Islands",
+        abbreviation: "MH"
+    },
+    {
+        title: "Maryland",
+        abbreviation: "MD"
+    },
+    {
+        title: "Massachusetts",
+        abbreviation: "MA"
+    },
+    {
+        title: "Michigan",
+        abbreviation: "MI"
+    },
+    {
+        title: "Minnesota",
+        abbreviation: "MN"
+    },
+    {
+        title: "Mississippi",
+        abbreviation: "MS"
+    },
+    {
+        title: "Missouri",
+        abbreviation: "MO"
+    },
+    {
+        title: "Montana",
+        abbreviation: "MT"
+    },
+    {
+        title: "Nebraska",
+        abbreviation: "NE"
+    },
+    {
+        title: "Nevada",
+        abbreviation: "NV"
+    },
+    {
+        title: "New Hampshire",
+        abbreviation: "NH"
+    },
+    {
+        title: "New Jersey",
+        abbreviation: "NJ"
+    },
+    {
+        title: "New Mexico",
+        abbreviation: "NM"
+    },
+    {
+        title: "New York",
+        abbreviation: "NY"
+    },
+    {
+        title: "North Carolina",
+        abbreviation: "NC"
+    },
+    {
+        title: "North Dakota",
+        abbreviation: "ND"
+    },
+    {
+        title: "Northern Mariana Islands",
+        abbreviation: "MP"
+    },
+    {
+        title: "Ohio",
+        abbreviation: "OH"
+    },
+    {
+        title: "Oklahoma",
+        abbreviation: "OK"
+    },
+    {
+        title: "Oregon",
+        abbreviation: "OR"
+    },
+    {
+        title: "Palau",
+        abbreviation: "PW"
+    },
+    {
+        title: "Pennsylvania",
+        abbreviation: "PA"
+    },
+    {
+        title: "Puerto Rico",
+        abbreviation: "PR"
+    },
+    {
+        title: "Rhode Island",
+        abbreviation: "RI"
+    },
+    {
+        title: "South Carolina",
+        abbreviation: "SC"
+    },
+    {
+        title: "South Dakota",
+        abbreviation: "SD"
+    },
+    {
+        title: "Tennessee",
+        abbreviation: "TN"
+    },
+    {
+        title: "Texas",
+        abbreviation: "TX"
+    },
+    {
+        title: "Utah",
+        abbreviation: "UT"
+    },
+    {
+        title: "Vermont",
+        abbreviation: "VT"
+    },
+    {
+        title: "Virgin Islands",
+        abbreviation: "VI"
+    },
+    {
+        title: "Virginia",
+        abbreviation: "VA"
+    },
+    {
+        title: "Washington",
+        abbreviation: "WA"
+    },
+    {
+        title: "West Virginia",
+        abbreviation: "WV"
+    },
+    {
+        title: "Wisconsin",
+        abbreviation: "WI"
+    },
+    {
+        title: "Wyoming",
+        abbreviation: "WY"
+    }
 ];
+
 const CitizenStatus = [
     { title: 'Citizen' },
     { title: 'Non-Citizen' },
