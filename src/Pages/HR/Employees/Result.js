8d42a28a54abf1b746ea47db9db1586174c8db0d
moiz-amin <liqteq.moiz@gmail.com>
2022-04-28 12:48:26 +0500

    employee and pdf and role acess

--- src/Pages/HR/Employees/Result.js
+++ src/Pages/HR/Employees/Result.js
@@ -215,7 +215,7 @@ const permission = {
   showPosition: [2, 5],
   showPay: [2, 5],
   showTerminate: [2, 5],
-  hrAdminOrSystemAdmin: [2, 5,6],
+  hrAdminOrSystemAdmin: [2, 5],
   attachfile:[2,5]
 };
 
@@ -974,7 +974,7 @@ const EmployeeResult = (props) => {
 
 
               {
-                permission?.position?.includes(+role_id) && (
+                permission?.showPosition?.includes(+role_id) && (
 
                   <div>
 
